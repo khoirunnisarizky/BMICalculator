f5f551d78c3d4279ac82b6fea8be6be177905757
Khoirunnisa' Rizky Noor Fatimah <49296015+khoirunnisarizky@users.noreply.github.com>
2023-03-19 16:01:31 +0700

    Initial Commit

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by khoirunnisa' rizky noor fatimah on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
